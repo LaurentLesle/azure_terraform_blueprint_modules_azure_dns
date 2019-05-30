f8dcf32c5652c257eb10ad7e28d2e6ae971a3f1f
Laurent <laurent.lesle@microsoft.com>
2019-05-30 23:56:40 +0800

    Upgrade to 0.12

--- dns_zone.tf
+++ dns_zone.tf
@@ -1,10 +1,11 @@
 resource "azurerm_dns_zone" "dns" {
-  count                             = "${length(keys(var.dns_zone))}"
-  
-  name                              = "${var.subdomain}${var.dns_zone["name"]}"
-  resource_group_name               = "${var.resource_group_name}"
-  zone_type                         = "${var.dns_zone["zone_type"]}"
+  count = length(keys(var.dns_zone))
 
-  resolution_virtual_network_ids    = ["${var.resolution_virtual_network_ids}"]
-  registration_virtual_network_ids  = ["${var.registration_virtual_network_ids}"]
+  name                = "${var.subdomain}${var.dns_zone["name"]}"
+  resource_group_name = var.resource_group_name
+  zone_type           = var.dns_zone["zone_type"]
+
+  resolution_virtual_network_ids   = var.resolution_virtual_network_ids
+  registration_virtual_network_ids = var.registration_virtual_network_ids
 }
+
--- variables.tf
+++ variables.tf
@@ -1,30 +1,26 @@
 variable "resource_group_name" {
-  
 }
 
 variable "dns_zone" {
-    description = "(Optional) Map of the DNS zone to create"
-    type = "map"
-    default = {}
+  description = "(Optional) Map of the DNS zone to create"
+  type        = map(string)
+  default     = {}
 }
 
 variable "subdomain" {
-    description = "(Optional) Sub-domain to add in the form of 'subname.'"
-    default = ""
+  description = "(Optional) Sub-domain to add in the form of 'subname.'"
+  default     = ""
 }
 
-
 variable "resolution_virtual_network_ids" {
-    description = "(Optional) A list of Virtual Network ID's that resolve records in this DNS zone. This field can only be set when zone_type is set to Private."
-    type = "list"
-    default = []
-    
+  description = "(Optional) A list of Virtual Network ID's that resolve records in this DNS zone. This field can only be set when zone_type is set to Private."
+  type        = list(string)
+  default     = []
 }
 
 variable "registration_virtual_network_ids" {
-    description = " (Optional) A list of Virtual Network ID's that register hostnames in this DNS zone. This field can only be set when zone_type is set to Private."
-    type = "list"
-    default = []
+  description = " (Optional) A list of Virtual Network ID's that register hostnames in this DNS zone. This field can only be set when zone_type is set to Private."
+  type        = list(string)
+  default     = []
 }
-
 
